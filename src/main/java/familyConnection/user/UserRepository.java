b3b54330eac43510eabdb915e6d50ec3af0a50af
2heunxun <seapeon@kyonggi.ac.kr>
2025-11-02 01:01:37 +0900

    [feature] init User Domain Repository Dev

---
+++ src/main/java/familyConnection/user/UserRepository.java
@@ -0,0 +1,10 @@
+package familyConnection.user;// familyConnection/security/user/UserRepository.java
+
+import familyConnection.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByKakaoId(String kakaoId);
+}
