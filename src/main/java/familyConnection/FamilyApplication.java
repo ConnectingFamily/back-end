5b61a4414d3af2e091ddc9f8997e20fc9d24dfd2
2heunxun <seapeon@kyonggi.ac.kr>
2025-11-02 00:59:10 +0900

    [feature] init setting

--- src/main/java/familyConnection/FamilyApplication.java
+++ src/main/java/familyConnection/FamilyApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package familyConnection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FamilyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FamilyApplication.class, args);
 	}
 
 }
