e71013a576bce181388565c819968cf78d64dea3
2heunxun <seapeon@naver.com>
2025-11-09 00:33:28 +0900

    [refector] spring doc using for JWT

--- src/main/java/familyConnection/global/config/OpenApiConfig.java
+++ src/main/java/familyConnection/global/config/OpenApiConfig.java
@@ -1,17 +1,35 @@
 package familyConnection.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI baseOpenAPI() {
         return new OpenAPI()
+                // 1) 전역으로 "이 API는 이 인증 방식을 쓴다" 선언
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                // 2) JWT Bearer 스키마 정의
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
                 .info(new Info()
                         .title("ConnectionFamily API")
                         .description("ConnectionFamily 백엔드 API 문서")
@@ -20,4 +38,3 @@ public class OpenApiConfig {
                 .externalDocs(new ExternalDocumentation().description("README"));
     }
 }
-
