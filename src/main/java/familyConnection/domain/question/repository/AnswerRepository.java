18f1a3e6c74bf57e24929cb1831d9f2ff31ca9eb
2heunxun <seapeon@naver.com>
2025-11-09 16:06:10 +0900

    [refector] Answer repository add

--- src/main/java/familyConnection/domain/question/repository/AnswerRepository.java
+++ src/main/java/familyConnection/domain/question/repository/AnswerRepository.java
@@ -1,10 +1,14 @@
 package familyConnection.domain.question.repository;
 
+import familyConnection.domain.family.entity.Family;
 import familyConnection.domain.question.entity.Answer;
 import familyConnection.domain.question.entity.DailyQuestion;
 import familyConnection.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +17,26 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
     Optional<Answer> findByDailyQuestionAndUser(DailyQuestion dailyQuestion, User user);
 
     List<Answer> findByDailyQuestion(DailyQuestion dailyQuestion);
+
+    List<Answer> findByUserAndFamilyAndSubmittedAtBetween(
+            User user,
+            Family family,
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+    Page<Answer> findByUserAndFamilyOrderBySubmittedAtDesc(
+            User user,
+            Family family,
+            Pageable pageable
+    );
+
+    // 카운팅이에요 그냥 MAX 로 반환했습니다.
+    long countByUserAndFamily(User user, Family family);
+
+    Optional<Answer> findByUserAndFamilyAndDailyQuestion_DailyQuestionId(
+            User user,
+            Family family,
+            Long dailyQuestionId
+    );
 }
