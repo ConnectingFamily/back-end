8993bd483b7d63785c93a54540d42589eb0b62b1
2heunxun <seapeon@naver.com>
2025-11-09 20:56:03 +0900

    [feature] Answer ai support dto devv

---
+++ src/main/java/familyConnection/domain/aianswerhelper/dto/AnswerDraftRequestDto.java
@@ -0,0 +1,9 @@
+package familyConnection.domain.aianswerhelper.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class AnswerDraftRequestDto {
+    private String content; // 사용자가 처음 쓴 답변
+}
---
+++ src/main/java/familyConnection/domain/aianswerhelper/dto/AnswerFinalizeRequestDto.java
@@ -0,0 +1,8 @@
+package familyConnection.domain.aianswerhelper.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AnswerFinalizeRequestDto {
+    private String finalContent; // 사용자가 진짜로 저장하려는 최종 답변
+}
