4cfb47892e68b6900befd31fe84a1c4751b51412
2heunxun <seapeon@naver.com>
2025-11-09 13:18:05 +0900

    [feature] Comment repository dev

---
+++ src/main/java/familyConnection/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package familyConnection.domain.comment.repository;
+
+import familyConnection.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
